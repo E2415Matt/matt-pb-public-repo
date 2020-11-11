31447eebeceba7f19155209c6cfb658c8a05603d
M. Altun <m.altun@MacBook-Pro.local>
2020-11-11 19:44:29 +0000

    intermediate refining / updates committed

---
+++ 201-dockerization-bookstore-api-on-python-flask-mysql/classnotes.sh
@@ -0,0 +1,3 @@
+# CRUD Applications --> Create Read/Retrive Update Delete
+# CRUD is an acronym for the four basic types of SQL commands: Create , Read , Update , Delete . Most applications have some kind of CRUD functionality, and we can assume that every programmer had to deal with CRUD at some point. A CRUD application is one that uses forms to get data into and out of a database.
+
